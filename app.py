3da42606b6018826142a79323d45c0ea965eb30b
Santiago <santiago.gutierrez02@correo.usa.edu.co>
2021-10-24 19:59:55 -0500

    Se agrega función para scrapping

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import json
+import urllib, urllib.request
+from urllib import request
 
 def handler(event, context):
     print("hello from zappa")
@@ -6,12 +8,12 @@ def handler(event, context):
     return {'status': 200}
 
 
-def descargarPagina():
-  with urllib.request.urlopen('https://www.elespectador.com/') as handler:
-    html = str(handler.read(), 'utf-8')
-    with open('el_espectador.html','w') as file_handler:
-        file_handler.write(html)
-  with urllib.request.urlopen('https://www.eltiempo.com/') as handler2:
-    html2 = str(handler2.read(), 'utf-8')
-    with open('eltiempo.html','w') as file_handler2:
-        file_handler2.write(html2)   
+def descargarPagina(event, context):
+    with urllib.request.urlopen('https://www.elespectador.com/') as handler:
+        html = str(handler.read(), 'utf-8')
+        with open('el_espectador.html','w') as file_handler:
+            file_handler.write(html)
+    with urllib.request.urlopen('https://www.eltiempo.com/') as handler2:
+        html2 = str(handler2.read(), 'utf-8')
+        with open('eltiempo.html','w') as file_handler2:
+            file_handler2.write(html2)   
