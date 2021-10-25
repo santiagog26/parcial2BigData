d0af8f70bbeef0bd72a525e446f5675f26fb0b33
Santiago <santiago.gutierrez02@correo.usa.edu.co>
2021-10-25 11:25:27 -0500

    Cambios en el scrapping inicial

--- noticias/app.py
+++ noticias/app.py
@@ -5,24 +5,25 @@ import json
 import boto3
 import time
 
-
-bucket="zappa-parcial2-descargas"
-
+bucket="zappa-parcial2-descargas-noticias"
 def handler(event,context):
+    localtime=time.localtime()
+    s3 = boto3.resource('s3')
+    
+    descargarPagina('El espectador', 'https://www.elespectador.com/', localtime, bucket, s3)
+    descargarPagina('El tiempo', 'https://www.eltiempo.com/' , localtime, bucket, s3)
+    
+    return {
+        "status_code":200
+        }
 
-	localtime=time.localtime()
-
-	s3 = boto3.resource('s3')
-	descargarPagina('https://www.elespectador.com/' , 'elespectador.html',localtime,bucket,s3)
-    descargarPagina('https://www.eltiempo.com/' , 'eltiempo.html',localtime,bucket,s3)
-
-	return {
-			"status_code":200
-		}
-
-def descargarPagina(url,archivo,localtime,bucketname,s3):
-  with urllib.request.urlopen(url) as handler:
-    html = str(handler.read(), 'utf-8')
-    with open(archivo,'w') as file_handler:
-        file_handler.write(html)
-     
+def descargarPagina(name, url, localtime, bucketname, s3):	
+	headers = {'User-Agent': 'Mozilla'}
+	r = requests.get(url, headers=headers)
+	filepath="/tmp/"+name+".txt"
+	f = open(filepath,"w")
+	print("Saving file from "+name)
+	f.write(r.text)
+	f.close()
+	path = 'headlines/raw/periodico='+name+'/year='+str(localtime.tm_year)+'/month='+str(localtime.tm_mon)+'/day='+str(localtime.tm_mday)+'/'+name+'.txt'
+	s3.meta.client.upload_file(filepath, bucketname, path)
